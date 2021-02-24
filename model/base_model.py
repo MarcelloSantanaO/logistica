f8b5292563656a4869859b0d4ba497dae18df9f0
Gustavo Sobreiro <gustavo.sobreiro@olist.com>
2021-02-24 15:23:56 -0300

    tests2 gpg2

--- model/base_model.py
+++ model/base_model.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class BaseModel(Base):
     __abstract__ = True
     id_ = Column('id', Integer, primary_key=True)
